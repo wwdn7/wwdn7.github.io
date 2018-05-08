d27560d91404d0b7cb31ece55794679e2d6a434a
Victor刘维 <33219309+wwdn7@users.noreply.github.com>
2018-05-08 15:42:55 +0800

    删除一些图片

--- sw.js
+++ sw.js
@@ -27,17 +27,7 @@ var imgArr = [
   '/static/images/7.jpg',
   '/static/images/8.jpg',
   '/static/images/9.jpg',
-  '/static/images/10.jpg',
-  '/static/images/11.jpg',
-  '/static/images/12.jpg',
-  '/static/images/13.jpg',
-  '/static/images/14.jpg',
-  '/static/images/15.jpg',
-  '/static/images/16.jpg',
-  '/static/images/17.jpg',
-  '/static/images/18.jpg',
-  '/static/images/19.jpg',
-  '/static/images/20.jpg'
+  '/static/images/10.jpg'
 ]
 caches.open(CACHE_NAME).then(function (cache) {
   cache.addAll(imgArr);
